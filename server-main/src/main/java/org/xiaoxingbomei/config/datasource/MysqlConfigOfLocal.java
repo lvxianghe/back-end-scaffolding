311462463659dacec7ed71aeead6ca1243aeac33
ysx <496745927@qq.com>
2024-07-29 00:37:42 +0800

    1、mybatis complete sql log

--- server-main/src/main/java/org/xiaoxingbomei/config/datasource/MysqlConfigOfLocal.java
+++ server-main/src/main/java/org/xiaoxingbomei/config/datasource/MysqlConfigOfLocal.java
@@ -10,6 +10,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.xiaoxingbomei.config.mybatis.MybatisInterceptor;
 import org.xiaoxingbomei.config.mybatis.MybatisLogInterceptor;
 
 import javax.sql.DataSource;
@@ -17,40 +18,45 @@ import javax.sql.DataSource;
 /**
  * 本地数据库连接配置
  */
+// 标注该类是一个配置类
 @Configuration
+// 指定 MyBatis 扫描的 Mapper 接口包路径，并指定使用的 SqlSessionTemplate
 @MapperScan(basePackages = "org.xiaoxingbomei.dao.localhost", sqlSessionTemplateRef = "localhostSqlSessionTemplate")
-public class MysqlConfigOfLocal
-{
+public class MysqlConfigOfLocal {
+
+    // 定义一个 DataSource Bean，并从配置文件中读取以 `spring.datasource.localhost` 为前缀的属性
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.localhost")
-    public DataSource localhostDataSource()
-    {
+    public DataSource localhostDataSource() {
         return DataSourceBuilder.create().build();
     }
 
-
+    // 定义一个 SqlSessionFactory Bean，依赖于 `localhostDataSource`
     @Bean
     @DependsOn("localhostDataSource")
-    public SqlSessionFactory localhostSqlSessionFactory() throws Exception
-    {
+    public SqlSessionFactory localhostSqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        // 设置数据源为 `localhostDataSource`
         factoryBean.setDataSource(localhostDataSource());
+        // 设置 MyBatis 插件，例如自定义的 MybatisLogInterceptor
         factoryBean.setPlugins(new Interceptor[]{new MybatisLogInterceptor()});
 
+        // 配置 MyBatis 的设置，例如在字段值为 null 时调用 setter 方法
         org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
         configuration.setCallSettersOnNulls(true);
         factoryBean.setConfiguration(configuration);
+
+        // 返回配置好的 SqlSessionFactory
         return factoryBean.getObject();
     }
 
-
+    // 定义一个 SqlSessionTemplate Bean，依赖于 `localhostSqlSessionFactory`
     @Bean
     @DependsOn("localhostSqlSessionFactory")
-    public SqlSessionTemplate localhostSqlSessionTemplate() throws Exception
-    {
+    public SqlSessionTemplate localhostSqlSessionTemplate() throws Exception {
+        // 创建并返回 SqlSessionTemplate
         SqlSessionTemplate template = new SqlSessionTemplate(localhostSqlSessionFactory());
         return template;
     }
-
 
 }
--- server-main/src/main/java/org/xiaoxingbomei/config/mybatis/MybatisInterceptor.java
+++ server-main/src/main/java/org/xiaoxingbomei/config/mybatis/MybatisInterceptor.java
@@ -1,152 +1,124 @@
 package org.xiaoxingbomei.config.mybatis;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.executor.Executor;
-import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.ParameterMapping;
-import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.type.TypeHandlerRegistry;
+
 import org.springframework.stereotype.Component;
-import org.xiaoxingbomei.aspect.ControllerLogAspectByPath;
-import org.xiaoxingbomei.entity.GlobalRequestContext;
 
-import java.text.DateFormat;
-import java.util.*;
-
-
-/**
- * mybatis log 拦截器
- */
 @Slf4j
 @Component
 @Intercepts({
         @Signature(type = Executor.class, method = "update", args = { MappedStatement.class, Object.class }),
         @Signature(type = Executor.class, method = "query", args = { MappedStatement.class, Object.class,
                 RowBounds.class, ResultHandler.class }) })
-public class MybatisLogInterceptor implements Interceptor
-{
+public class MybatisInterceptor implements Interceptor {
+    private Properties properties;
 
     @Override
-    public Object intercept(Invocation invocation) throws Throwable
-    {
-        // 记录开始时间
-        long startTime = System.currentTimeMillis();
-
-        log.info("MybatisLogInterceptor start");
-
-        // 获取请求上下文信息
-        GlobalRequestContext requestContext = ControllerLogAspectByPath.getGlobalRequestContext();
+    public Object intercept(Invocation invocation) throws Throwable {
         MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
         Object parameter = null;
-        // 继续执行并记录结束时间
-        if (invocation.getArgs().length > 1)
-        {
+        if (invocation.getArgs().length > 1) {
             parameter = invocation.getArgs()[1];
         }
         String sqlId = mappedStatement.getId();
         BoundSql boundSql = mappedStatement.getBoundSql(parameter);
         Configuration configuration = mappedStatement.getConfiguration();
-
-        String sql = showSql(configuration, boundSql);
-
-        long endTime = System.currentTimeMillis();
-        long executionTime = endTime - startTime;
-
-        // 美化打印样式
-        log.info("\n----------------------------------------------------------\n\t{}{}{}{}{}{}{}{}",
-                " << mybatis complete sql log >>",
-                "\n\t [request ip]   \t:    \t"  + requestContext.getIp(),
-                "\n\t [request url]  \t:    \t"  + requestContext.getUrl(),
-                "\n\t [request path] \t:    \t"  + requestContext.getClassName()+requestContext.getMethodName(),
-                "\n\t [sql spendTime]\t:    \t"  + executionTime+"ms",
-                "\n\t [sql Id]       \t:    \t"  + sqlId,
-                "\n\t [complete sql] \t: \n \t"  + sql,
-                "\n----------------------------------------------------------\n");
-
-
-        return invocation.proceed();
+        Object returnValue = null;
+        long start = System.currentTimeMillis();
+        returnValue = invocation.proceed();
+        long end = System.currentTimeMillis();
+        long time = (end - start);
+        if (time > 1) {
+            String sql = getSql(configuration, boundSql, sqlId, time);
+            log.info(sql);
+        }
+        return returnValue;
     }
 
+    public static String getSql(Configuration configuration, BoundSql boundSql, String sqlId, long time) {
+        String sql = showSql(configuration, boundSql);
+        StringBuilder str = new StringBuilder(100);
+        str.append("：\n");
+        str.append(sqlId);
+        str.append(":\n执行的SQL语句为---->:\n");
+        str.append(sql);
+        str.append("\n----执行时间:");
+        str.append(time);
+        str.append("ms");
+        return str.toString();
+    }
 
     private static String getParameterValue(Object obj) {
         String value = null;
-
-        // 判断对象是否为字符串类型
         if (obj instanceof String) {
-            // 如果是字符串类型，则用单引号包裹字符串值
             value = "'" + obj.toString() + "'";
         } else if (obj instanceof Date) {
-            // 如果是日期类型，则格式化日期并用 `to_date` 包裹
             DateFormat formatter = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.CHINA);
             value = "to_date('" + formatter.format(obj) + "','yyyy-MM-dd hh24:mi:ss')";
         } else {
-            // 如果对象不为空，则直接将其转换为字符串
             if (obj != null) {
                 value = obj.toString();
             } else {
-                // 如果对象为空，则将值设为空字符串
                 value = "";
             }
+
         }
-        return value; // 返回格式化后的参数值
+        return value;
     }
 
-
     public static String showSql(Configuration configuration, BoundSql boundSql) {
-        // 获取 SQL 语句的参数对象
         Object parameterObject = boundSql.getParameterObject();
-        // 获取 SQL 语句中的参数映射
         List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
-        // 获取并格式化 SQL 语句，去除多余的空白字符
         String sql = boundSql.getSql().replaceAll("[\\s]+", " ");
-
-        // 检查是否存在参数映射并且参数对象不为空
         if (parameterMappings.size() > 0 && parameterObject != null) {
-            // 获取 TypeHandler 注册器
             TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
-
-            // 检查参数对象是否有对应的 TypeHandler
             if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
-                // 如果有，则直接替换第一个 "?" 为参数值
                 sql = sql.replaceFirst("\\?", getParameterValue(parameterObject));
-            } else {
-                // 否则创建参数对象的 MetaObject
-                MetaObject metaObject = configuration.newMetaObject(parameterObject);
 
-                // 遍历参数映射
+            } else {
+                MetaObject metaObject = configuration.newMetaObject(parameterObject);
                 for (ParameterMapping parameterMapping : parameterMappings) {
                     String propertyName = parameterMapping.getProperty();
-
-                    // 如果参数对象中有对应的 getter 方法
                     if (metaObject.hasGetter(propertyName)) {
-                        // 获取参数值并替换 SQL 中的 "?"
                         Object obj = metaObject.getValue(propertyName);
                         sql = sql.replaceFirst("\\?", getParameterValue(obj));
                     } else if (boundSql.hasAdditionalParameter(propertyName)) {
-                        // 如果参数对象中没有对应的 getter 方法，但 BoundSql 中有额外的参数
-                        // 获取额外参数并替换 SQL 中的 "?"
                         Object obj = boundSql.getAdditionalParameter(propertyName);
                         sql = sql.replaceFirst("\\?", getParameterValue(obj));
                     }
                 }
             }
         }
-        return sql; // 返回格式化后的 SQL 语句
+        return sql;
     }
 
-
     @Override
-    public Object plugin(Object target)
-    {
+    public Object plugin(Object target) {
         return Plugin.wrap(target, this);
     }
 
     @Override
-    public void setProperties(Properties properties) {}
+    public void setProperties(Properties properties0) {
+        this.properties = properties0;
+    }
 }
